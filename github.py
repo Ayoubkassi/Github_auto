0861993e4c0409404c4f8b048f671dd0ca636350
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-09-23 21:59:53 +0100

    Finish the project

--- github.py
+++ github.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
 from time import sleep
+import subprocess
+import shlex
+
 
 project_name = input("Enter the project name : ")
-# username = input("Enter Github username : ")
-# password = input("Enter Github password : ")
+#username = input("Enter Github username : ")
+password = input("Enter Github password : ")
+
+link = "https://github.com/Ayoubkassi/"+project_name+".git"
 
 
 class GithubBot:
@@ -22,10 +27,15 @@ class GithubBot:
         sleep(2)
         repo_field = self.driver.find_element_by_xpath(
             '//*[@id="repository_name"]').send_keys(project_name)
-        sleep(4)
+        sleep(2)
+        check_butt = self.driver.find_element_by_xpath(
+            '//*[@id="repository_auto_init"]').click()
+        sleep(2)
         create_butt = self.driver.find_element_by_xpath(
             '//*[@id="new_repository"]/div[4]/button').click()
         sleep(4)
+        self.driver.close()
 
 
-my_bot = GithubBot('Ayoubkassi', 'IKRAM.hiba2004')
+my_bot = GithubBot('Ayoubkassi', password)
+subprocess.call(['bash', 'github.sh', link, project_name])
---
+++ github.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone $1
+cd $2
+atom .
+exit 1
